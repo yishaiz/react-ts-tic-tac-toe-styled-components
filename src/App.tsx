3dbc951f042d836b2d6c7194bbd94bc877e5a101
yishai <yishai5555@gmail.com>
2025-03-27 18:38:45 +0200

    02-styled-component

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  // const [count, setCount] = useState<number>(0);
-
-  return <h1>Tic Tac Toe</h1>;
-}
-
-export default App;
---
+++ src/components/Game.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+type LayoutProps = {
+  gap: number;
+};
+
+
+function Game() {
+  return (
+    <div>
+      <h2>Hello Tic tac Toe</h2>
+      
+    </div>
+  );
+}
+
+export default Game;
+
