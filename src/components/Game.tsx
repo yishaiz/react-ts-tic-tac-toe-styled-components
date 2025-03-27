6ddfe561a81693f7529e8501f87cb68d35fb7d8f
yishai <yishai5555@gmail.com>
2025-03-27 18:38:53 +0200

    03-row

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,15 +5,25 @@ type LayoutProps = {
   gap: number;
 };
 
+const Row = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.gap}px;
+`;
 
 function Game() {
   return (
     <div>
       <h2>Hello Tic tac Toe</h2>
-      
+      <Row gap={2} />
     </div>
   );
 }
 
 export default Game;
 
+// const Column = styled.div<LayoutProps>`
+//   display: flex;
+//   flex-direction: column;
+//   gap: ${(props) => props.gap}px;
+// `;
